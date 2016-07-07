3ef0228a9a985a2a2c582411e37525d05be5c386
Josh Bohde <josh@joshbohde.com>
2016-07-07 16:11:14 -0500

    run test suite as a test suite.

--- tests.hs
+++ tests.hs
@@ -1,13 +1,13 @@
-import Test.HUnit
-import Language.Go.Tests.Lexer
-import Language.Go.Tests.Parser
-import Language.Go.Tests.ParseStatements
-import Language.Go.Tests.Pretty
-import Language.Go.Tests.Types
+import           Language.Go.Tests.Lexer
+import           Language.Go.Tests.Parser
+import           Language.Go.Tests.ParseStatements
+import           Language.Go.Tests.Pretty
+import           Language.Go.Tests.Types
+import           Test.HUnit
 
 main :: IO Counts
 main = runTestTT $ TestList (testsLexer
-                          ++ testsParser
-                          ++ testsParseStmts
-                          ++ testsPretty
-                          ++ testsTypes )
+                             ++ testsParser
+                             ++ testsParseStmts
+                             ++ testsPretty
+                             ++ testsTypes )
